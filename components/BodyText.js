c27a7f57acee8f70546dce8cfa737dbced2e020d
ushair <ushairansari@gmail.com>
2021-07-21 16:14:22 +0530

    Custom Wrapper Components Global Styles

---
+++ components/BodyText.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text,StyleSheet} from 'react-native'
+
+export const BodyText = props => <Text style={styles.body}>{props.children}</Text>
+
+const styles = StyleSheet.create({
+    body:{
+        fontFamily:'open-sans'
+    }
+})
